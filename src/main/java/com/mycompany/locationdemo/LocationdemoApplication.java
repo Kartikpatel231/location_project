862f6f010229df40acd1df184f77556e1a40fb19
Kartikpatel231 <kartikpatel1950@gmail.com>
2022-07-04 12:59:02 +0530

    location

---
+++ src/main/java/com/mycompany/locationdemo/LocationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.locationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationdemoApplication.class, args);
+	}
+
+}
